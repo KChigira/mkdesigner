fc19cf4a4aff6a2e0f8d638782505254d94807a9
KChigira <kyoujin2009kutar@gmail.com>
2023-06-19 18:58:35 +0900

    v0.0.1

--- mkdesigner/__init__.py
+++ mkdesigner/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "0.0.1"
+__copyright__    = 'Copyright (C) 2018 CHIGIRA Koki'
+__license__      = 'MIT License'
+__author__       = 'CHIGIRA Koki'
+__author_email__ = 's211905s@st.go.tuat.ac.jp'
+__url__          = 'http://github.com/KChigira/mkdesigner'
--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 from mkdesigner.__init__ import __version__
 
 setup(name='mkdesigner',
-      version="0.0.1",
-      description='Tools for designing DNA markers and their PCR primers from NGS data',
+      version=__version__,
+      description='Genome-wide design of markers for PCR-based genotyping from NGS data.',
       author='Koki Chigira',
       author_email='s211905s@st.go.tuat.ac.jp',
       url='https://github.com/KChigira/mkdesigner/',
-      license='GPL',
-      packages=["mkdesigner"],
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+        'pandas',
+      ],
       entry_points={'console_scripts': [
             'mkvcf = mkdesigner.mkvcf:main',
             'mkprimer = mkdesigner.mkprimer:main',
