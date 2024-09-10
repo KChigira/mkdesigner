9e345556cfda0cabd1af5e9e0dd67ad8550e05c8
KChigira <kyoujin2009kutar@gmail.com>
2024-09-10 09:17:45 +0900

    240910 modified

--- mkdesigner/__init__.py
+++ mkdesigner/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 __copyright__    = 'Copyright (C) 2023 CHIGIRA Koki'
 __license__      = 'MIT License'
 __author__       = 'CHIGIRA Koki'
-__author_email__ = 's211905s@st.go.tuat.ac.jp'
+__author_email__ = 'kyoujin2009kutar@gmail.com'
 __url__          = 'http://github.com/KChigira/mkdesigner'
